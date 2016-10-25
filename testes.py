e61ede8bc0d44e89b35c03701faa1d4e0a47fe42
Sidney <sid-reis@hotmail.com>
2016-10-25 20:59:31 -0200

    Update test

--- testes.py
+++ testes.py
@@ -1,45 +1,28 @@
-import numpy
 import decimal
 import math
 
-golden = decimal.Decimal((1 + 5 ** 0.5) / 2)
+proporcaoAurea = decimal.Decimal((1 + 5 ** 0.5) / 2)
+valorAproximado = decimal.Decimal(1.0)
+iteracao = 1
+
+while (abs((decimal.Decimal(valorAproximado) - \
+    decimal.Decimal(proporcaoAurea))) \
+    > 0.00000000000000009) and (iteracao <= 1000000):
+
+    valorAproximado = math.sqrt(1+valorAproximado)
+    iteracao = iteracao+1
+
+print 'iteracoes necessarias: '+ str(iteracao)
 
 
-g2 = decimal.Decimal(1.0)
-iteracao2 = 1
-
-while (abs((decimal.Decimal(g2) - decimal.Decimal(golden))) > 0.00000000000000000009):
-    g2 = math.sqrt(1+g2)
-    iteracao2 = iteracao2+1
-    #print 'rai i: ' + str(iteracao2)
-
-print 'raizes: '+ str(iteracao2)
 
 
-g1 = decimal.Decimal(1.0)
-iteracao1 = 1
-
-while (abs((decimal.Decimal(g1) - decimal.Decimal(golden))) > 0.00000000000000000009) and iteracao1 <= 1000000:
-    g1 = decimal.Decimal(decimal.Decimal(1.0) + decimal.Decimal(1.0)/decimal.Decimal(g1))
-    # print decimal.Decimal(decimal.Decimal(golden) - decimal.Decimal(g1))
-    #print "g1: " + str(g1)
-    #print "golden: " + str(golden)
-    #print "golden - g1: " + str(decimal.Decimal(golden) - decimal.Decimal(g1))
-    #print decimal.Decimal(g1)
-    iteracao1 = iteracao1+1
-    #print 'frac i: ' + str(iteracao1)
-
-print 'fracoes: ' + str(iteracao1)
 
 # g1 = decimal.Decimal(1.0)
-# g2 = decimal.Decimal(1.0)
+# iteracao1 = 1
 
-# while (abs((decimal.Decimal(g1) - decimal.Decimal(golden))) > 0.000001):
-#     g2 = g1
-#     g1 = decimal.Decimal(decimal.Decimal(g)1 + decimal.Decimal(g2))
-#     # print decimal.Decimal(decimal.Decimal(golden) - decimal.Decimal(g1))
-#     #print "g1: " + str(g1)
-#     #print "golden: " + str(golden)
-#     #print "golden - g1: " + str(decimal.Decimal(golden) - decimal.Decimal(g1))
-#     print decimal.Decimal(g1)
+# while (abs((decimal.Decimal(g1) - decimal.Decimal(proporcaoAurea))) > 0.00000000000000000009) and iteracao1 <= 1000000:
+#     g1 = decimal.Decimal(decimal.Decimal(1.0) + decimal.Decimal(1.0)/decimal.Decimal(g1))
+#     iteracao1 = iteracao1+1
 
+# print 'fracoes: ' + str(iteracao1)
